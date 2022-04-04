36e1dfda8d084378f9fb63d240561c4204790e3b
yanweida <yanweida@xiaomi.com>
2022-04-04 16:04:07 +0800

    fix: 封装sql执行函数

---
+++ blog1/src/conf/db.js
@@ -0,0 +1,30 @@
+const env = process.env.NODE_ENV // 环境变量参数
+
+
+// 配置
+let MYSQL_CONF
+
+if(env === 'dev') {
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: 'choubuyaolian@@2',
+        port: '3306',
+        database: 'myblog'
+    }
+}
+
+if(env === 'production') {
+    // 生成环境 应该是真实服务器配置
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: 'choubuyaolian@@2',
+        port: '3306',
+        database: 'myblog'
+    }
+}
+
+module.exports = {
+    MYSQL_CONF
+} 
